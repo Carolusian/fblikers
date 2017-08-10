721036f2a2812e99ae4ede86c165f2044e18352f
Charlie Chen <pkufashuo400@gmail.com>
2017-08-10 17:00:46 +0800

    Update test case for login

--- test_sample.py
+++ test_sample.py
@@ -5,15 +5,20 @@
 # Date: 30.07.2017
 # Last Modified Date: 30.07.2017
 
+import os
 import unittest
-from fblikers.users import user_from_dict
+from selenium.webdriver.common.by import By
+from fblikers.users import user_from_dict, load_users
 from fblikers.exceptions import UnsupportedPlatformException
+from fblikers.actions import login
+
+
+SAMPLE_USERS_FILE = os.path.join(os.path.dirname(__file__), 'sample-users.csv')
 
 
 class FBlikersTest(unittest.TestCase):
-
     def setUp(self):
-        pass
+        self.sample_users = load_users(SAMPLE_USERS_FILE)
 
     def test_user_loader(self):
         with self.assertRaises(UnsupportedPlatformException) as context:
@@ -25,6 +30,7 @@ class FBlikersTest(unittest.TestCase):
             user_from_dict(row)
         self.assertTrue('Unsupported platform' in str(context.exception))
 
-
     def test_login(self):
-        pass
+        browsers = [login(user) for user in self.sample_users]
+        for browser in browsers:
+            browser.find_elements(B
